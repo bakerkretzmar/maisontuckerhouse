5182eae622997fe08c38edf70dc4c4d67cd97064
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-16 16:24:09 -0400

    Frontpage cards

--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -34,46 +34,28 @@
     </main>
   </div>
   <div class="row cards">
-    <?php $featured_pages = get_posts([
-      'post_type' => 'page',
-      'numberposts' => '-1',
-      'meta_key' => 'featured_page',
-      'meta_value' => true,
+    <?php $cards = get_posts([
+      'post_type' => 'attachment',
+      'numberposts' => '3',
+      'tax_query' => [
+        [
+          'taxonomy' => 'media_tag',
+          'field'    => 'slug',
+          'terms'    => 'frontpage_card',
+        ],
+      ],
     ]);
 
-    if ( $featured_pages ) {
-      foreach ($featured_pages as $card) {
+    if ( $cards ) {
+      foreach ($cards as $card) {
         echo '<div class="col-md-4"><div class="card">';
-        if ( has_post_thumbnail( $card->ID ) ) {
-          $card_img = get_post_thumbnail_id( $card->ID );
-          echo '<img class="card-img-top" src="' . get_the_post_thumbnail_url( $card->ID, 'full' ) . '" alt="' . get_post_meta( $card_img, '_wp_attachment_image_alt', true) . '">';
-        }
+        echo '<img class="card-img-top" src="' . wp_get_attachment_image_src( $card->ID, 'full' )[0] . '" alt="' . apply_filters( 'the_title', $card->post_title ) . '">';
         echo '<div class="card-block">';
-        echo '<h5 class="card-title">' . get_the_title( $card->ID ) . '</h5>';
-        echo '<p class="card-text">' . get_the_excerpt( $card->ID ) . '</p>';
-        echo '<div class="btn btn-primary custom-buttom red"><a href="' . get_the_permalink( $card->ID ) . '">Learn More</a></div>';
+        echo '<h5 class="card-title">' . apply_filters( 'the_title', $card->post_title ) . '</h5>';
+        echo apply_filters( 'the_content', $card->post_excerpt );
+        echo '<div class="btn btn-primary custom-buttom red"><a href="' . esc_url( $card->media_link ) . '">More</a></div>';
         echo '</div></div></div>';
       }
     } ?>
-    <div class="col-md-4">
-      <div class="card">
-        <img class="card-img-top" src="http://maisontuckerhouse.dev/wp-content/uploads/2017/08/andy-kerr-153378.jpg" alt="Card image cap">
-        <div class="card-block">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <div class="btn btn-primary custom-buttom red"><a href="#">Learn More</a></div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card">
-        <img class="card-img-top" src="http://maisontuckerhouse.dev/wp-content/uploads/2017/08/arvin-febry-302935.jpg" alt="Card image cap">
-        <div class="card-block">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <div class="btn btn-primary custom-buttom red"><a href="#">Learn More</a></div>
-        </div>
-      </div>
-    </div>
-  </div><!-- /.content -->
+  </div>
 </div><!-- /.wrap -->
