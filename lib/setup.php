5e05d896f394947b58df3295e54dbe2a2ad18857
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-16 15:48:28 -0400

    Add frontpage cards sidebar

--- lib/setup.php
+++ lib/setup.php
@@ -76,6 +76,15 @@ function widgets_init() {
   ]);
 
   register_sidebar([
+    'name'          => __('Front Page Cards Area', 'maisontuckerhouse'),
+    'id'            => 'sidebar-frontpage-cards',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
+
+  register_sidebar([
     'name'          => __('Footer 1', 'maisontuckerhouse'),
     'id'            => 'sidebar-footer-l',
     'before_widget' => '<section class="widget %1$s %2$s">',
