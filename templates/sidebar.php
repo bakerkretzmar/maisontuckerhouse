98212f6971f2afec8df16c46e34cffe6735f68aa
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-16 16:57:01 -0400

    Add secondary sidebar display logic

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,5 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php if ( get_field('sidebar_select') == 'Secondary' ) {
+  dynamic_sidebar('sidebar-secondary');
+} else {
+  dynamic_sidebar('sidebar-primary');
+} ?>
