5e09fa5a8eebaab15daa734bb3b6a1ddb79a46a0
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-08-24 16:48:14 -0400

    Remove Secondary sidebar

--- lib/setup.php
+++ lib/setup.php
@@ -67,15 +67,6 @@ function widgets_init() {
   ]);
 
   register_sidebar([
-    'name'          => __('Secondary', 'maisontuckerhouse'),
-    'id'            => 'sidebar-secondary',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
-
-  register_sidebar([
     'name'          => __('Footer 1', 'maisontuckerhouse'),
     'id'            => 'sidebar-footer-l',
     'before_widget' => '<section class="widget %1$s %2$s">',
