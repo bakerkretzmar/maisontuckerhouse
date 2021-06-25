0a5e023290ce410d6f8fe3bc6e877425c9bd5a42
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-06-24 22:06:07 -0400

    Tweak sponsor styles

--- templates/footer-sponsors.php
+++ templates/footer-sponsors.php
@@ -1,28 +1,25 @@
-<footer id="logoArea" class="container-fluid">
-    <p><?php echo mth_trans('Thanks to our sponsors and partners:'); ?></p>
-    <div class="row no-gutters">
-      <?php
-        $logos = get_posts([
-          'post_type' => 'attachment',
-          'numberposts' => '-1',
-          'tax_query' => [
-            [
-              'taxonomy' => 'media_tag',
-              'field'    => 'slug',
-              'terms'    => 'sponsor_logo',
-            ],
-          ],
-        ]);
-        foreach ($logos as $logo) {
-          $meta = wp_get_attachment_metadata( $logo->ID );
-          $classes = 'col-sm-6 col-md-4';
-          if ( ( $meta['width'] / $meta['height'] ) > 3 ) {
-            $classes = 'col-sm-12 col-md-12 wide';
-          } else if ( ( $meta['width'] / $meta['height'] ) > 2 ) {
-            $classes = 'col-sm-12 col-md-8';
-          }
-          echo '<div class="logo ' . $classes . '"><a class="sponsor" href="' . esc_url( $logo->media_link ) . '" target="_blank">' . wp_get_attachment_image( $logo->ID, 'full' ) . '</a></div>';
-        };
-      ?>
+<footer class="tw_w-full tw_py-6 tw_px-4">
+    <p class="tw_text-center tw_mb-2"><?php echo mth_trans('Thanks to our sponsors and partners:'); ?></p>
+
+    <div class="tw_flex tw_flex-wrap tw_-mx-4">
+        <?php
+            $logos = get_posts([
+                'post_type' => 'attachment',
+                'numberposts' => '-1',
+                'tax_query' => [
+                    [
+                        'taxonomy' => 'media_tag',
+                        'field' => 'slug',
+                        'terms' => 'sponsor_logo',
+                    ],
+                ],
+            ]);
+
+            foreach ($logos as $logo) {
+                $meta = wp_get_attachment_metadata($logo->ID);
+
+                echo '<div class="tw_m-8 tw_text-center"><a href="' . esc_url($logo->media_link) . '" target="_blank">' . wp_get_attachment_image($logo->ID, 'full') . '</a></div>';
+            }
+        ?>
     </div>
 </footer>
