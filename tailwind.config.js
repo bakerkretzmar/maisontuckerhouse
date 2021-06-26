4bf3916e0f8ba37a227737ddae22e1a67b60debc
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-06-26 18:53:33 -0400

    Wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const defaults = require('tailwindcss/defaultTheme');
+
 module.exports = {
     mode: 'jit',
     prefix: 'tw_',
-    purge: ['./assets/scripts/*.js', './lib/*.php', './templates/*.php', './*.php'],
+    purge: [
+        './assets/scripts/*.js',
+        './lib/*.php',
+        './templates/*.php',
+        './*.php',
+    ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"Proxima Nova"', ...defaults.fontFamily.sans],
+                calluna: ['"Calluna Sans"', ...defaults.fontFamily.sans],
+                museo: ['"Museo Sans"', ...defaults.fontFamily.sans],
+            },
+        },
     },
     corePlugins: {
         preflight: false,
