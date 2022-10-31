7888e3ac6cc9eaaec1372ef0d53643b43a3da9b8
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-10-31 18:17:46 -0400

    Wip site header

--- web/app/themes/mth/resources/views/sections/header.blade.php
+++ web/app/themes/mth/resources/views/sections/header.blade.php
@@ -1,11 +1,24 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
-
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+<header class="flex items-center gap-2 p-2 font-proxima bg-gray-50">
+    <a href="{{ home_url('/') }}">
+        <img src="@asset('images/logo-og_wordmark-narrow.svg')" class="h-28" alt="{{ __('Tucker House logo', 'mth') }}" />
+    </a>
+    <div>
+        <h4 class="text-2xl font-calluna text-red-900 italic">{{ $siteDescription }}</h4>
+        @if (has_nav_menu('primary_navigation'))
+            <nav class="flex uppercase font-semibold text-green-700" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+                {!! wp_nav_menu([
+                    'theme_location' => 'primary_navigation',
+                    'menu_class' => 'flex gap-4',
+                    'echo' => false,
+                ]) !!}
+            </nav>
+        @endif
+    </div>
+    <div>
+        @foreach (pll_the_languages(['raw' => true]) as $locale)
+            @unless ($locale['current_lang'])
+                <a href="{{ $locale['url'] }}">{{ $locale['name'] }}</a>
+            @endunless
+        @endforeach
+    </div>
 </header>
