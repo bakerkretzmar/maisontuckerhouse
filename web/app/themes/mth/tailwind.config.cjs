f2b84562768943852e9f3bec9e6445687bab0cd1
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-10-31 18:17:38 -0400

    Set up fonts

--- web/app/themes/mth/tailwind.config.cjs
+++ web/app/themes/mth/tailwind.config.cjs
@@ -1,10 +1,18 @@
 const defaults = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
     theme: {
         extend: {
-            colors: {}, // Extend Tailwind's default colors
+            colors: {
+                gray: colors.stone,
+            },
+            fontFamily: {
+                museo: ['museo-sans', ...defaults.fontFamily.sans],
+                calluna: ['calluna-sans', ...defaults.fontFamily.sans],
+                proxima: ['proxima-nova', ...defaults.fontFamily.sans],
+            },
         },
     },
 };
